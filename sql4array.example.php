6ab414972a8df1c86d5192816740637ce3dc556f
bluelovers <codelovers@users.sourceforge.net>
2012-02-28 01:10:01 +0800

    .

--- sql4array.example.php
+++ sql4array.example.php
@@ -1,7 +1,6 @@
 <?PHP
 
-
-/*
+/**
  * Project:		Absynthe sql4array
  * File:		sql4array.example.php5
  * Author:		Absynthe <sylvain@abstraction.fr>
@@ -18,20 +17,20 @@ require "./sql4array.class.php";
 
 for ($i = 0; $i < 20; $i++)
 {
-	$array[$i][id]	= rand(0, 20);
-	$array[$i][foo]	= md5(rand(0, 10000));
+	$array[$i][id] = rand(0, 20);
+	$array[$i][foo] = md5(rand(0, 10000));
 }
 
 
-$sql	= new sql4array();
+$sql = new sql4array();
 
 $sql->createFromGlobals();
 
-$a		= $sql->query("SELECT id, foo FROM array");
-$b		= $sql->query("SELECT id, foo FROM array WHERE id > 10");
-$c		= $sql->query("SELECT id AS i, foo AS f FROM array WHERE i > 10");
-$d		= $sql->query("SELECT id AS i, foo AS f FROM array WHERE i > 10 AND f LIKE '%a%'");
-$e		= $sql->query("SELECT id AS iiiiii, foo AS fooooooooo FROM array WHERE iiiiii != 10");
+$a = $sql->query("SELECT id, foo FROM array");
+$b = $sql->query("SELECT id, foo FROM array WHERE id > 10");
+$c = $sql->query("SELECT id AS i, foo AS f FROM array WHERE i > 10");
+$d = $sql->query("SELECT id AS i, foo AS f FROM array WHERE i > 10 AND f LIKE '%a%'");
+$e = $sql->query("SELECT id AS iiiiii, foo AS fooooooooo FROM array WHERE iiiiii != 10");
 
 var_dump($a);
 var_dump($b);
